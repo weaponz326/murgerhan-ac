53d5936e996ad8d309bf719a8a1fa26185341cf9
Weps <weaponz326@gmail.com>
2023-07-05 06:33:52 +0000

    installed angular/fire

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+
+import { environment } from 'src/environments/environment';
+
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
 import { AdminSetupComponent } from './admin-setup/admin-setup.component';
@@ -25,7 +31,8 @@ import { LogoutComponent } from './logout/logout.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.development.ts
@@ -0,0 +1,12 @@
+export const environment = {
+    production: false,
+    firebase: {
+        apiKey: "AIzaSyBT10IX6Y8ZJroL6jI75raWprbC01ATXY0",
+        authDomain: "weapons-7d089.firebaseapp.com",
+        projectId: "weapons-7d089",
+        storageBucket: "weapons-7d089.appspot.com",
+        messagingSenderId: "337928193877",
+        appId: "1:337928193877:web:99e1793c54e271ce6591dc",
+        measurementId: "G-SH0HR63N6Y"               
+    }
+};
